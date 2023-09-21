da6efda82727162970c8557ef2729c329f8e7324
Azizbek Fayzullayev <azizbekfayzullayev04@gmail.com>
2023-09-21 14:36:44 +0500

    My first commit3

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Registration1</title>
+    <title>Registration</title>
 </head>
 <body>
     <?php
